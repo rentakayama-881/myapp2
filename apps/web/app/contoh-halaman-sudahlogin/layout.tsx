a257df9f32df070d9f0ecac78d5b59dd2276c1c4
alep <alep@nodehost.ru>
2026-03-05 19:18:53 +0300

    Revert "feat(web): add auth UI pages and logged-in member shell"
    This reverts commit 99a41dcdcd6a704d3666365e2b8bcbde28266d82.

--- apps/web/app/contoh-halaman-sudahlogin/layout.tsx
+++
@@ -1,9 +0,0 @@
-import { MemberShell } from '../../components/app-shell/member-shell';
-
-export default function MemberLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
-  return <MemberShell>{children}</MemberShell>;
-}
